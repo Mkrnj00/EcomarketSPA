beef37ed78e53830fad712042fe10942e8457559
Francisca Espinoza Cárdenas <waxopez@gmail.com>
2025-05-17 00:04:12 -0400

    agregue repository

---
+++ ecomarket/src/main/java/com/ecomarketspa_vm/cl/ecomarket/repository/ProductoRepository.java
@@ -0,0 +1,7 @@
+package com.ecomarketspa_vm.cl.ecomarket.repository;
+import com.ecomarketspa_vm.cl.ecomarket.model.Producto;
+import org.springframework.orm.jpa.repository.Jparepository;
+import org.springframework.stereotype.Repository;
+public interface ProductoRepository {
+
+}
