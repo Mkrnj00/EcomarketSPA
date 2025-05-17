91c8a599cb120d34e8a2908afd50f709a9a0d96a
Francisca Espinoza Cárdenas <waxopez@gmail.com>
2025-05-17 00:30:34 -0400

    arregle la m mayusculas XD

--- ecomarket/src/main/java/com/ecomarketspa_vm/cl/ecomarket/repository/ProductoRepository.java
+++ ecomarket/src/main/java/com/ecomarketspa_vm/cl/ecomarket/repository/ProductoRepository.java
@@ -1,5 +1,5 @@
 package com.ecomarketspa_vm.cl.ecomarket.repository;
-import com.ecomarketspa_vm.cl.ecomarket.Model.Producto;
+import com.ecomarketspa_vm.cl.ecomarket.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
