9497a1c514c978f846a3d58a51f3a15224c6ae69
Mkrnj00 <matiaskuroyt@gmail.com>
2025-05-17 00:04:31 -0400

    Branch matias creada

--- ecomarket/src/main/java/Model/Producto.java
+++ ecomarket/src/main/java/Model/Producto.java
@@ -1,5 +1,38 @@
-package Controller;
+package Model;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Table(name = "Producto")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Producto {
+@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    @Column(unique = true, length = 13, nullable = false)
+    private String codigoBarras;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String descripcion;
+
+    @Column(nullable = true)
+    private Date fechaVencimiento;
+
+    @Column(nullable = false)
+    private Double precio;
 }
